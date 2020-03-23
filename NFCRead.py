f68c0e0ba811179db1344ca21d17e4923a162e4b
Jack Egan <jackegan248@gmail.com>
2020-03-23 01:07:31 +0000

    Added TAG Reader

---
+++ NFCRead.py
@@ -0,0 +1,28 @@
+import nxppy
+import time
+import ndef
+import REST
+
+mifare = nxppy.Mifare()
+
+# Print card UIDs as they are detected
+while True:
+    try:
+        uid = mifare.select()
+
+        ndef_data = mifare.read_ndef()
+        # Parse NDEF data
+        ndef_records = list(ndef.message_decoder(ndef_data))
+
+        tagData = "UID: " + uid + "NDEF: "
+
+        for x in ndef_records:
+            tagData = tagData + x + " "
+
+         sendPost(tagData)
+
+    except nxppy.SelectError:
+        # SelectError is raised if no card is in the field.
+        pass
+
+    time.sleep(1)
--- REST.py
+++ REST.py
@@ -4,6 +4,3 @@ def sendPost(tagData):
     URL = "http://54.93.224.148/TAG/upload"
     PARAMS = {'data':tagData}
     r = requests.post(url = URL, data = PARAMS)
-
-sendPost("This is a test function call from pi")
-sendPost("Raspberry PI")
