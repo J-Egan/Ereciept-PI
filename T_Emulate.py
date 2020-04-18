c0ad3b05c85401aec435ed7978938defb71d9bad
Jack Egan <jackegan248@gmail.com>
2020-04-18 17:11:40 +0100

    Update T_Emulate.py

--- T_Emulate.py
+++ T_Emulate.py
@@ -2,19 +2,20 @@ import os
 import signal
 import subprocess
 
-process = subprocess.Popen(['explorenfc-cardemulation'],stdout=subprocess.PIPE, universal_newlines=True)
+process = subprocess.Popen(['explorenfc-cardemulation', '-u'],stdout=subprocess.PIPE, universal_newlines=True)
 
 while True:
     print(process.poll())
     line = process.poll()
     # line = process.stdout.readline()
     print(line)
-    if "userID:" in line:
-        startOf = line.find("userID")
-        userID = line[startOf:]
-        print(userID)
+    if line is not None:
+        if "userID:" in line:
+            startOf = line.find("userID")
+            userID = line[startOf:]
+            print(userID)
 
-        # Kill subprocess now
-        os.killpg(os.getpgid(process.pid), signal.SIGTERM)
-    else:
-        print("No UserID detected!")
+            # Kill subprocess now
+            os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+        else:
+            print("No UserID detected!")
